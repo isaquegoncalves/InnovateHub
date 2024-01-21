d53f0921e4a0d37b9ae7e4ca8f5b694928d27a4c
isaquegoncalves <114412444+isaquegoncalves@users.noreply.github.com>
2024-01-20 23:54:23 -0300

    Versão 1.2
    Novidades:
    -Adição de logomarca.
    
    -Animações nas descrições: Quem somos, Perspectivas e Recursos.
    
    -Script de LiveScroll.
    
    -Breve responsividade Geral.

--- script/animated_title.js
+++ script/animated_title.js
@@ -1,5 +1,4 @@
-//----TITULO_ANIMADO----
-
+//---------TIMERS--------------
 //======<H1>=======
 document.addEventListener("DOMContentLoaded", function(){
     const titulo = document.getElementsByClassName('titulo_animado');
@@ -9,12 +8,13 @@ document.addEventListener("DOMContentLoaded", function(){
         }
     }, 800);
 
-//======<h2_titulo_desc>=======
+//======<h2_SUBTITULO>=======
     setTimeout(function(){
         const h2titulo = document.getElementById('titulo_desc');
         h2titulo.classList.add('active');
     }, 1000);
 
+//============<DIV_CAPA-DE-FUNDO===========>
     setTimeout(function(){
         const foto = document.getElementById('foto_home');
         foto.classList.add('active');
@@ -24,4 +24,39 @@ document.addEventListener("DOMContentLoaded", function(){
         const h2titulo = document.getElementById('menu');
         h2titulo.classList.add('active');
     }, 2000);
+});
+
+//============<Detector de LiveScroll>===========
+window.addEventListener('scroll', function(){
+    //------------Descriçoes<ARTICLE>------------
+    var tags = document.querySelectorAll('.scroll-detec');
+    tags.forEach(function(tag) {
+        var rect = tag.getBoundingClientRect();
+        if (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= window.innerHeight &&
+            rect.right <= window.innerWidth
+        ) {
+            tag.classList.add('active');
+        } else {
+            tag.classList.remove('active');
+        }
+    }); 
+
+    //------------Fotos_do_<ARTICLE>------------
+    var fotos = document.querySelectorAll('.fotos_articles');
+    fotos.forEach(function(img) {
+        var rect = img.getBoundingClientRect();
+        if (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= window.innerHeight &&
+            rect.right <= window.innerWidth
+        ) {
+            img.classList.add('active');
+        } else {
+            img.classList.remove('active');
+        }
+    }); 
 });
